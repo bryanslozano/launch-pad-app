fda0b7071a1ffcc6add3f39edce1480c7be592d4
bryanslozano <bryan@ad-apt.com>
2025-02-07 13:24:18 -0800

    modified:   app/routes/app.looker-report.jsx

--- app/routes/app.looker-report.jsx
+++ app/routes/app.looker-report.jsx
@@ -5,7 +5,7 @@ export default function LookerReport() {
     <div>
       <h1>Looker Studio Report</h1>
       <iframe
-        src="https://lookerstudio.google.com/s/vdUlZG-2pSo"
+        src="https://lookerstudio.google.com/embed/reporting/f908de8b-0215-4c85-8d8e-dada0efedaa2/page/p_ig7dmtugfd"
         width="100%"
         height="600"
         frameBorder="0"
