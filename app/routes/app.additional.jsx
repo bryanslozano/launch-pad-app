526b55872672470c5f566e111def61f07ea22693
bryanslozano <bryan@ad-apt.com>
2025-02-07 13:16:35 -0800

    modified:   app/routes/app.additional.jsx

--- app/routes/app.additional.jsx
+++ app/routes/app.additional.jsx
@@ -81,3 +81,14 @@ function Code({ children }) {
     </Box>
   );
 }
+
+<div>
+<h1>Looker Studio Report</h1>
+<iframe
+  src="https://lookerstudio.google.com/s/vdUlZG-2pSo"
+  width="100%"
+  height="600"
+  frameBorder="0"
+  allowFullScreen
+></iframe>
+</div>
