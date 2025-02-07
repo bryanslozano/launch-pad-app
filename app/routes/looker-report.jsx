9b210722d69deab5c6e7a56e1267d84e49da7243
bryanslozano <bryan@ad-apt.com>
2025-02-07 12:43:01 -0800

    Please enter the commit message for your changes. Lines starting 	new file:   .dockerignore 	new file:   .editorconfig 	new file:   .eslintignore 	new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   .graphqlrc.js 	new file:   .npmrc 	new file:   .prettierignore 	new file:   .vscode/extensions.json 	new file:   CHANGELOG.md 	new file:   Dockerfile 	new file:   README.md 	new file:   prisma/migrations/20240530213853_create_session_table/migration.sql 	new file:   prisma/schema.prisma 	new file:   public/favicon.ico 	new file:   remix.config.js 	new file:   shopify.app.toml 	new file:   shopify.web.toml 	new file:   tsconfig.json 	new file:   vite.config.js

---
+++ app/routes/looker-report.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function LookerReport() {
+  return (
+    <div>
+      <h1>Looker Studio Report</h1>
+      <iframe
+        src="https://lookerstudio.google.com/reporting/dc7578cf-c11b-48b7-a0a8-30e0ca55e331"
+        width="100%"
+        height="600"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
