a629f4a8f6f7b62967e2b2db8eb7cc184e296ee0
bryanslozano <bryan@ad-apt.com>
2025-02-20 16:31:17 -0800

    figuring out vite issues

--- app/routes/app.report-test.jsx
+++ app/routes/app.report-test.jsx
@@ -1,26 +1,13 @@
-import {
-    Box,
-    Card,
-    Layout,
-    Link,
-    List,
-    Page,
-    Text,
-    BlockStack,
-  } from "@shopify/polaris";
-  import { TitleBar } from "@shopify/app-bridge-react";
-  
-  export default function ReportTest() {
-    return (
-        <Page>
-      <TitleBar title="Test page" />
-      <iframe
-        src="https://lookerstudio.google.com/embed/reporting/f908de8b-0215-4c85-8d8e-dada0efedaa2/page/p_ig7dmtugfd"
-        width="100%"
-        height="1000px"
-        frameBorder="0"
-        allowFullScreen
-      ></iframe>
-      </Page>
-    );
+import React from 'react';
+import MyChart from '../components/MyChart';
+
+function ReportTest() {
+  return (
+    <div>
+      <h1>Report Test</h1>
+      <MyChart />
+    </div>
+  );
 }
+
+export default ReportTest;
